bab088d35cf9b182e1fafc9c69f0c1658775f596
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-09-03 19:48:58 +0300

    Update play.py

--- ArchMusic/utils/decorators/play.py
+++ ArchMusic/utils/decorators/play.py
@@ -1,4 +1,15 @@
+#
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
+#
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
 from ArchMusic import YouTube, app
@@ -10,114 +21,108 @@ from ArchMusic.utils.database import (get_cmode, get_lang,
                                        is_served_private_chat)
 from ArchMusic.utils.database.memorydatabase import is_maintenance
 from ArchMusic.utils.inline.playlist import botplaylist_markup
-import time
 
-# ----------------------
-# play_logs stub (yer tutucu)
-# ----------------------
-async def play_logs(message, streamtype, duration=None, filesize=None, start_time=None, end_time=None, requester=None):
-    """
-    Test ve debug amaçlı basit play_logs fonksiyonu.
-    Gerçek ArchMusic play_logs fonksiyonu yoksa bunu kullan.
-    """
-    await message.reply_text(
-        f"Playing {streamtype}...\n"
-        f"Duration: {duration}\n"
-        f"Filesize: {filesize}\n"
-        f"Start: {start_time}\n"
-        f"End: {end_time}\n"
-        f"Requested by: {requester}"
-    )
 
-# ----------------------
-# PlayWrapper Dekoratörü
-# ----------------------
 def PlayWrapper(command):
-    async def wrapper(client, message, *args, **kwargs):
-        # Bakım modu kontrolü
+    async def wrapper(client, message):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
-                return await message.reply_text("Bot bakımda. Lütfen bir süre bekleyin...")
-
-        # Özel bot modu
+                return await message.reply_text(
+                    "Bot bakımda. Lütfen bir süre bekleyin..."
+                )
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
                 await message.reply_text(
-                    "**Özel Müzik Botu**\nYalnızca sahibinden gelen yetkili sohbetler için."
+                    "**Özel Müzik Botu**\n\nYalnızca sahibinden gelen yetkili sohbetler için. Önce sahibimden sohbetinize izin vermesini isteyin."
                 )
                 return await app.leave_chat(message.chat.id)
-
-        # Komut otomatik silme
         if await is_commanddelete_on(message.chat.id):
-            try: await message.delete()
-            except: pass
-
-        # Dil
+            try:
+                await message.delete()
+            except:
+                pass
         language = await get_lang(message.chat.id)
         _ = get_string(language)
-
-        # Telegram medyası veya YouTube linki
-        audio_telegram = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
-        video_telegram = (message.reply_to_message.video or message.reply_to_message.document) if message.reply_to_message else None
+        audio_telegram = (
+            (
+                message.reply_to_message.audio
+                or message.reply_to_message.voice
+            )
+            if message.reply_to_message
+            else None
+        )
+        video_telegram = (
+            (
+                message.reply_to_message.video
+                or message.reply_to_message.document
+            )
+            if message.reply_to_message
+            else None
+        )
         url = await YouTube.url(message)
-
-        # Playlist veya stream kontrolü
-        if audio_telegram is None and video_telegram is None and url is None:
+        if (
+            audio_telegram is None
+            and video_telegram is None
+            and url is None
+        ):
             if len(message.command) < 2:
+                if "stream" in message.command:
+                    return await message.reply_text(_["str_1"])
                 buttons = botplaylist_markup(_)
                 return await message.reply_photo(
                     photo=PLAYLIST_IMG_URL,
                     caption=_["playlist_1"],
                     reply_markup=InlineKeyboardMarkup(buttons),
                 )
-
-        # Kanal veya chat
+        if message.sender_chat:
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="How to Fix this? ",
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(
+                _["general_4"], reply_markup=upl
+            )
         if message.command[0][0] == "c":
             chat_id = await get_cmode(message.chat.id)
-            if chat_id is None: return await message.reply_text(_["setting_12"])
+            if chat_id is None:
+                return await message.reply_text(_["setting_12"])
             try:
                 chat = await app.get_chat(chat_id)
-            except: return await message.reply_text(_["cplay_4"])
+            except:
+                return await message.reply_text(_["cplay_4"])
             channel = chat.title
         else:
             chat_id = message.chat.id
             channel = None
-
-        # Yetki
         playmode = await get_playmode(message.chat.id)
         playty = await get_playtype(message.chat.id)
-        if playty != "Everyone" and message.from_user.id not in SUDOERS:
-            admins = adminlist.get(message.chat.id)
-            if not admins or message.from_user.id not in admins:
-                return await message.reply_text(_["play_4"])
-
-        # Video kontrolü
-        video = True if message.command[0][0] == "v" or "-v" in message.text else None
-        fplay = True if message.command[0][-1] == "e" and await is_active_chat(chat_id) else None
-
-        # Duration, FileSize, Start/End Time
-        duration = None
-        filesize = None
-        start_time = time.time()
-        end_time = None
-
-        if url:  # YouTube
-            info = await YouTube.get_info(url)
-            duration = info.get("duration")
-            filesize = info.get("filesize")
-        elif audio_telegram:
-            duration = audio_telegram.duration
-            filesize = audio_telegram.file_size
-        elif video_telegram:
-            duration = getattr(video_telegram, "duration", None)
-            filesize = video_telegram.file_size
-
-        if duration:
-            end_time = start_time + duration
-
-        requester = message.from_user.id
-
-        # Komutu çalıştır
+        if playty != "Everyone":
+            if message.from_user.id not in SUDOERS:
+                admins = adminlist.get(message.chat.id)
+                if not admins:
+                    return await message.reply_text(_["admin_18"])
+                else:
+                    if message.from_user.id not in admins:
+                        return await message.reply_text(_["play_4"])
+        if message.command[0][0] == "v":
+            video = True
+        else:
+            if "-v" in message.text:
+                video = True
+            else:
+                video = True if message.command[0][1] == "v" else None
+        if message.command[0][-1] == "e":
+            if not await is_active_chat(chat_id):
+                return await message.reply_text(_["play_18"])
+            fplay = True
+        else:
+            fplay = None
         return await command(
             client,
             message,
@@ -128,25 +133,6 @@ def PlayWrapper(command):
             playmode,
             url,
             fplay,
-            duration=duration,
-            filesize=filesize,
-            start_time=start_time,
-            end_time=end_time,
-            requester=requester,
-            *args,
-            **kwargs
         )
 
     return wrapper
-
-# ----------------------
-# play_command Fonksiyonu
-# ----------------------
-@PlayWrapper
-async def play_command(client, message, _, chat_id, video, channel, playmode, url, fplay, *args, **kwargs):
-    """
-    Müzik oynatma komutu
-    """
-    streamtype = "video" if video else "audio"
-
-    return await
