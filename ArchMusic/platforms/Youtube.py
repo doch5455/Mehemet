c2211fcdae989de38e555b6b6417241cde08fd55
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-09-03 22:42:45 +0300

    Update Youtube.py

--- ArchMusic/platforms/Youtube.py
+++ ArchMusic/platforms/Youtube.py
@@ -9,7 +9,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ArchMusic.utils.database import is_on_off
+
 from ArchMusic.utils.formatters import time_to_seconds
 
 
